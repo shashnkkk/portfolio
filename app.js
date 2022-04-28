2aee3860ecd1c0855b00bedaa7d1bd27b773e2c2
Shashank Singh Chauhan <shashank.vchauhan@gmail.com>
2022-04-28 11:32:37 +0530

    made the design a lil bit responsive and fixed previos issues

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
-const sections = document.querySelectorAll(".section");
-const sectBtns = document.querySelectorAll(".controlls");
-const sectBtn = document.querySelectorAll(".control");
-const allSections = document.querySelector(".main-content");
+// const sections = document.querySelectorAll(".section");
+// const sectBtns = document.querySelectorAll(".controls");
+// const sectBtn = document.querySelectorAll(".control");
+// const allSections = document.querySelector(".main-content");
 
-function PageTransistions() {
-  //Button Click Active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        " "
-      );
-      this.className += " active-btn";
+// function PageTransistions() {
+//   //Button Click Active class
+//   for (let i = 0; i < sectBtn.length; i++) {
+//     sectBtn[i].addEventListener("click", function () {
+//       let currentBtn = document.querySelectorAll(".active-btn");
+//       currentBtn[0].className = currentBtn[0].className.replace(
+//         "active-btn",
+//         " "
+//       );
+//       this.className += " active-btn";
+//     });
+//   }
+
+//   //sections active
+//   allSections.addEventListener("click", (e) => {
+//     const id = e.target.dataset.id;
+//     if (id) {
+//       //remove selected  from other buttons
+//       sectBtns.forEach((btn) => {
+//         btn.classList.remove("active");
+//       });
+//       e.target.classList.add("active");
+//       //hide other sections
+//       sections.forEach((section) => {
+//         section.classList.remove("active");
+//       });
+//       const element = document.getElementById(id);
+//       element.classList.add("active");
+//     }
+//   });
+//   document.querySelector(".theme-btn").addEventListener("click", () => {
+//     document.body.classList.toggle("light-mode");
+//   });
+// }
+
+// PageTransistions();
+
+(function () {
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
     });
-  }
-
-  //sections active
-  allSections.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    if (id) {
-      //remove selected  from other buttons
-      sectBtns.forEach((btn) => {
-        btn.classList.remove("active");
-      });
-      e.target.classList.add("active");
-      //hide other sections
-      sections.forEach((section) => {
-        section.classList.remove("active");
-      });
-      const element = document.getElementById(id);
-      element.classList.add("active");
-    }
   });
   document.querySelector(".theme-btn").addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
   });
-}
-
-PageTransistions();
+})();
