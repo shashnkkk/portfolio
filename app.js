96c78174a235cf3a961c8e25527dd6ba6e71c5c5
Shashank Singh Chauhan <shashank.vchauhan@gmail.com>
2022-05-12 22:31:31 +0530

    changed home from section to header again because somethhing was not working

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ $("#ripple").ripples({
 $(window).on("load", function () {
   setTimeout(function () {
     $(".loading").hide();
-  }, 11000);
+  }, 11);
 });
 
 // $(document).ready(function () {
